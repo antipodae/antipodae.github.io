11c20594ae2e91fdd4d566f5c67fb8b2098273f3
Mathieu Clabaut <mathieu@clabaut.net>
2017-02-04 00:10:27 +0100

    Correct display of article

--- conf.py
+++ conf.py
@@ -291,7 +291,7 @@ SHOW_UNTRANSLATED_POSTS = True
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
-LOGO_URL = 'images/logo150.png'
+LOGO_URL = '/images/logo150.png'
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set this to False.
@@ -1016,10 +1016,10 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
 
 # Show link to source for the posts?
 # Formerly known as HIDE_SOURCELINK (inverse)
-# SHOW_SOURCELINK = True
+SHOW_SOURCELINK = False
 # Copy the source files for your pages?
 # Setting it to False implies SHOW_SOURCELINK = False
-# COPY_SOURCES = True
+COPY_SOURCES = False
 
 # Modify the number of Post per Index Page
 # Defaults to 10
